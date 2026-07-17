771cf5b8f9a937faa765a5ff31ee3eeb2fc3976e
Ashish Yadav <ashish150105@gmail.com>
2026-07-17 18:32:36 +0530

    Update CORS for Vercel

--- api.py
+++ api.py
@@ -11,7 +11,10 @@ app = FastAPI(title="AI Resume Matcher")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://talent-iq-ai-olive.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
